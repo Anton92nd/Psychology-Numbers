6b5e8c86b8af9e71bacfd02af3a7c4ad68c5e1c5
= <=>
2015-04-16 23:21:30 +0400

    add label and button to MainForm

--- Psychology-Numbers/MainForm.Designer.cs
+++ Psychology-Numbers/MainForm.Designer.cs
@@ -28,12 +28,47 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+            this.mainButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainButton
+            // 
+            this.mainButton.Location = new System.Drawing.Point(124, 253);
+            this.mainButton.Name = "mainButton";
+            this.mainButton.Size = new System.Drawing.Size(233, 79);
+            this.mainButton.TabIndex = 0;
+            this.mainButton.Text = "";
+            this.mainButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13F);
+            this.label1.Location = new System.Drawing.Point(120, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 21);
+            this.label1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 370);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mainButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
 		#endregion
+
+        private System.Windows.Forms.Button mainButton;
+        private System.Windows.Forms.Label label1;
 	}
 }
 
--- Psychology-Numbers/MainForm.cs
+++ Psychology-Numbers/MainForm.cs
@@ -1,20 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Psychology_Numbers
 {
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			InitializeComponent();
-		}
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // other properties mainButton and label1 in InitializeComponent
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            label1.Text = @"Доброго времени суток." + '\n' +
+                          @"Наша дружная команда в лице Медведя," + '\n' +
+                          @"Камня и Фуха предлагает вам поройти данное тестирование.";
+            mainButton.Text = @"Готовы?";
+            mainButton.BackColor = Color.SkyBlue;
+            mainButton.Click += MainButtonClick;
+        }
+
+        private void MainButtonClick(object sender, EventArgs e)
+        {
+            label1.Text = @"Вам предстоит пройти 4 этапа.";
+            mainButton.BackColor = Color.MediumSeaGreen;
+            mainButton.Text = @"Начали!";
+            mainButton.Click += Go_Click;
+        }
+
+        private void Go_Click(object sender, EventArgs e)
+        {
+            Console.Write(@"green button presed!");
+        }
+
 	}
 }
